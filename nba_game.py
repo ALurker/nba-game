7cad4cc7342f1a75eec1df6567afd054261357d0
ALurker <ALurker@outlook.com>
2019-11-23 12:22:13 -0600

    Initial Feedback

--- nba_game.py
+++ nba_game.py
@@ -1,16 +1,9 @@
 import random
+import re
 
 #prints out the name in a string rather than a list
 def show_name(lst):
-    name = ""
-    for i in lst:
-        i += " "
-        name += i
-
-    return name
-
-
-
+    return " ".join(lst)
 
 #this function gets all the stats of the player choosen
 def stats(player):
@@ -20,7 +13,6 @@ def stats(player):
     #Stephen curry is not in a list
     if type(player) != list:
         player = player.split()
-    
 
     shooting_splits = []
     for line in data:
@@ -38,12 +30,8 @@ def stats(player):
 
     return shooting_splits
 
-
-
-
 #chance_of_making will have the function of the shooting splits in it
 def chance_of_making(player,type_of_shot):
-    
     shooting_splits = stats(player)
 
     #three different shots
@@ -51,16 +39,10 @@ def chance_of_making(player,type_of_shot):
         #I manually have the three rating in the making_three function
         #shooting splits has all the ratings
         return making_three(shooting_splits)
-
     elif type_of_shot.lower() == "midrange":
         return making_midrange(shooting_splits)
-
     elif type_of_shot.lower() == "layup":
         return making_layup(shooting_splits)
-
-
-
-
 
 #chance of getting a steal
 #calculates the steal rate of a player
@@ -83,18 +65,11 @@ def turnover(player_1,team_1_players,team_2_players):
         current_ball_defender = team_2_players[x]
         shooting_splits = stats(current_ball_defender)
         return making_steal(shooting_splits)
-
-        
     elif player_1 in team_2_players:
         x = team_2_players.index(player_1)
         current_ball_defender = team_1_players[x]
         shooting_splits = stats(current_ball_defender)
         return making_steal(shooting_splits)
-
-
-
-
-
 
 def making_three(stats):
     #this correctly gets the three pointer rating
@@ -116,9 +91,6 @@ def making_three(stats):
     else:
         return score
 
-    
-
-
 def making_midrange(stats):
     midrange_rating = stats[1][2]
     #convert the string into an integer
@@ -136,8 +108,6 @@ def making_midrange(stats):
     else:
         return score
 
-
-
 def making_layup(stats):
     layup_rating = stats[2][2]
     #convert the string into an integer
@@ -154,8 +124,6 @@ def making_layup(stats):
         return score + 2 
     else:
         return score
-    
-
 
 #I have to get the steal rating of the defender and not the current ball handler
 def making_steal(stats):
@@ -177,113 +145,93 @@ def making_steal(stats):
     #I will assume the player with the deflection will have control of the ball
     if make_steal >= chance:
         return False
-
     else:
         return True
-
-
-
 
 #this function will give the likelihood of a rebound after a player misses a shot
 #how should i decide on how rebounding works????
 #defensive and offensive rebounding might have to be in two different functions
 def getting_defensive_rebound(stats,opposing_team):
-
     opposing_team_pg = opposing_team[0]
     opposing_team_sg = opposing_team[1]
     opposing_team_sf = opposing_team[2]
     opposing_team_pf = opposing_team[3]
     opposing_team_c = opposing_team[4]
 
-
     shooting_splits_pg = stats(opposing_team_pg)
     shooting_splits_sg = stats(opposing_team_sg)
     shooting_splits_sf = stats(opposing_team_sf)
     shooting_splits_pf = stats(opposing_team_pf)
     shooting_splits_c = stats(opposing_team_c)
-
-
+    ## HELP: what is this pass for?
     pass
 
-
-
-
 def getting_offensive_rebound(stats,current_team):
-
     current_team_pg = current_team[0]
     current_team_sg = current_team[1]
     current_team_sf = current_team[2]
     current_team_pf = current_team[3]
     current_team_c = current_team[4]
 
-
     shooting_splits_pg = stats(current_team_pg)
     shooting_splits_sg = stats(current_team_sg)
     shooting_splits_sf = stats(current_team_sf)
     shooting_splits_pf = stats(current_team_pf)
     shooting_splits_c = stats(current_team_c)
-
-
+    ## HELP: what is this pass for?
     pass
-
-
-
-
 
 #team_1_players ['Stephen Curry ', "D'Angelo Russell ", 'Klay Thompson ', 'Draymond Green ', 'Kevon Looney ']
 #team_2_players ['Ricky Rubio ', 'Devin Booker ', 'Mikal Bridges ', 'Kelly Oubre Jr. ', 'Deandre Ayton ']
 #team_1_players ['Stephen Curry ', "D'Angelo Russell ", 'Klay Thompson ', 'Draymond Green ', 'Kevon Looney ']
 #team_2_players ['Ricky Rubio ', 'Devin Booker ', 'Mikal Bridges ', 'Kelly Oubre Jr. ', 'Deandre Ayton ']
 def play(current_ball_handler,action,team_1_players,team_2_players):
-
     stays = True
     team_1 = team_1_players
     team_2 = team_2_players
 
     while action.lower() == "pass" and stays == True:
-
         #this gets the right team
         if current_ball_handler in team_1_players:
             current_team = team_1
         else:
             current_team = team_2
 
+        ## HOMEWORK:
+        #  Instead of having people type in the names of the people to pass
+        #  to, try to build a menu prompting people for input.
 
+        ## EXAMPLE:
+        #  A) Lebron James
+        #  B) Anthony Davis
+        #  ...
+        #  Who do you want to pass it to:
+        #  and then the user of your program can just type in "A" instead of
+        #  Lebron James
         current_ball_handler = input("Who do you want pass it to:")
         #current_ball_handler = current_ball_handler.split()
-        current_ball_handler += " "
         ##########
+
         while current_ball_handler not in current_team:
             current_ball_handler = input("Who do you want pass it to:")
-            current_ball_handler += " "
-
 
         #run the turnover function here to see if the pass was successful 
         stays = turnover(current_ball_handler,team_1_players,team_2_players)
 
-
-
         if stays == True:
             #this makes it into a list
             current_ball_handler = current_ball_handler.split()
-            print(show_name(current_ball_handler) + "can either pass or shoot")
-            action = input("What does " + show_name(current_ball_handler) + "choose to do: ")
-
+            print(current_ball_handler + " can either pass or shoot")
+            action = input("What does " + current_ball_handler + " choose to do: ")
 
     if stays == False:
         #the ball has to go to the other team
         return 0
-
-
-    
     elif action.lower() == "drive":
         pass
 
         #my number one priority
         #I have to work on the drive option!!!
-
-
-
     elif action.lower() == "shoot":
         print("You can shoot a layup, midrange, or three")
         type_of_shot = input("What type of shot do you want to shoot: ")
@@ -292,74 +240,68 @@ def play(current_ball_handler,action,team_1_players,team_2_players):
 
         #I have to put the rebounding function here
 
-
-
-
-
-
-
-
-def winner(team_1_points,team_2_points,team_1_lst,team_2_lst):
-
+def winner(team_1_points,team_2_points,team_1_name,team_2_name):
     if team_1_points > team_2_points:
-        print(show_name(team_1_lst),"has won")
-
+        print("{} has won".format(team_1_name))
     elif team_1_points < team_2_points:
-        print(show_name(team_2_lst), "has won")
-
+        print("{} has won".format(team_2_name))
     else:
-        print(show_name(team_1_lst),"and", show_name(team_2_lst), "have tied")
-       
-
-
-
+        print("{} and {} have tied".format(team_1_name, team_2_name))
 
 def main():
-
-    data = open("nba.txt","r")
+    team_file = open("nba.txt","r")
 
     #Now I want the user to choose which team they want to play as
     desired_team_1 = input("What team do you want to play as? ")
     desired_team_2 = input("What team do you want to go against? ")
 
-    desired_team_1 = desired_team_1.split()
-    desired_team_2 = desired_team_2.split()
-
-
-    team_1_lst = []
-    team_2_lst = []
     team_1_players = []
     team_2_players = []
 
-    for line in data:
-        x = line.split()
-        if x != []:
-            #this gets the team names
-            if x[0] == "Team:" and x[1:] == desired_team_1:
-                team_1_lst.append(x[1:])
-                x = data.readline()
-                x = x.split()
-                while x != []:
-                    team_1_players.append(show_name(x[1:]))
-                    x = data.readline()
-                    x = x.split()
+    ## HOMEWORK:
+    #  Convert this into a function that just returns all_teams
+    all_teams = {}
+    current_team_name = None
+    ## HOMEWORK:
+    #  Research Regular Expressions and how to use them
+    ## HINTS:
+    #  ^       == Start of Line
+    #  $       == End of Line
+    #  ()      == Capture Group
+    #  (?P<x>) == Named Capture Group with Name of "x"
+    team_extractor = re.compile(r"^Team: (?P<team_name>.*)$")
+    for line in team_file:
+        clean_line = line.strip()
+        extraction = team_extractor.match(clean_line)
+        if extraction:
+            team_name = extraction.group('team_name')
+            all_teams[team_name] = []
+        elif not clean_line == "":
+            # that means we are on a player clean_line
+            all_teams[team_name].append(clean_line.split(": ")[1])
 
-            elif x[0] == "Team:" and x[1:] == desired_team_2:
-                team_2_lst.append(x[1:]) 
-                x = data.readline()
-                x = x.split()
-                while x != []:
-                    team_2_players.append(show_name(x[1:]))
-                    x = data.readline()
-                    x = x.split()
+    ## HOMEWORK:
+    #  below are the same exact code only with different desired_team_x
+    #  variables, how could we clean this up so that we don't type out the same code
+    #  twice?
+    if desired_team_1 in all_teams:
+        team_1_players = all_teams[desired_team_1]
+    else:
+        print("{} is not a valid team!".format(desired_team_1))
+        exit()
 
+    if desired_team_2 in all_teams:
+        team_2_players = all_teams[desired_team_2]
+    else:
+        print("{} is not a valid team!".format(desired_team_2))
+        exit()
+            
+    print("team_1", desired_team_1)
+    print("team_2", desired_team_2)
 
-    #to make this into a 1D list
-    team_1_lst = team_1_lst[0]
-    team_2_lst = team_2_lst[0]
-    #print("TEAM",team_1_players)
-    print("team_1",show_name(team_1_lst))
-    print("team_2",show_name(team_2_lst))
+    ## HOMEWORK:
+    #  Please convert this to using "format" on strings instead of multiple
+    #  parameters to a print statement
 
     #this seperates the teams to their respective teams
     #I can make this neater later
@@ -370,34 +312,40 @@ def main():
     team_1_points = 0
     team_2_points = 0
     
-
-    
     #in this scenario team 1 starts with the ball
     #shows the current status of the game
     #need to keep track of the time left in the game
+
+    ## HELP: Why are we going to a range of 5? Are there only 5 rounds?
+    ## HOMEWORK:
+    #  This section emulates 5 rounds of play, with 1 round being defined
+    #  as the both teams having a turn with the ball.
+    #  You genericise the actions by re-assigning current_x variables.
+
+    #  Instead of genericising within the loop, could you convert this to a generic
+    #  function that you pass in the the teams, with the first team being the current
+    #  and the second being the opposing?
+
+    ## FUTURE HOMEWORK:
+    #  Research Classes and develop a Team class, which can have an array
+    #  of players as one attribute and a name as another attribute
     for i in range(5):
         current_ball_handler = team_1_players[0]
-        current_team = show_name(team_1_lst)
+        current_team = desired_team_1
         current_team_score = team_1_points
-
         
         print("The current team with the ball is the", current_team)
-        print("The current ball handler is",current_ball_handler)
-
-
+        print("The current ball handler is", current_ball_handler)
         
         #for now lets assume that the action is to shoot
-        print(current_ball_handler + "can either pass or shoot")
-        action = input("What does " + current_ball_handler + "choose to do: ")
-
-
+        print("{} can either pass or shoot".format(current_ball_handler))
+        action = input("What does {} choose to do: ".format(current_ball_handler))
+        
         #going to make it tell if the player missed the shot or not
         before_the_shot = team_1_points
 
-
         team_1_points += play(current_ball_handler,action,team_1_players,team_2_players)
-
-
+        
         #going to make it tell if the player missed the shot or not
         ##########DOES NOT DIFFERENTIATE BETWEEN TURNOVER AND MISSED SHOTS
         if before_the_shot < team_1_points:
@@ -405,38 +353,24 @@ def main():
         else:
             print(current_ball_handler + "missed the shot")
 
-
-        print(show_name(team_1_lst) + "has", team_1_points, "points")
-
-
-
-        ########################
-        ########################
-
-
-
+        print("{} has {} points".format(desired_team_1, team_1_points))
+        
         print("Now the other team has the ball")
         current_ball_handler = team_2_players[0]
-        current_team = show_name(team_2_lst)
+        current_team = desired_team_2
         current_team_score = team_2_points
-
         
         print("The current team with the ball is the", current_team)
         print("The current ball handler is",current_ball_handler)
-
-
         
         #for now lets assume that the action is to shoot
         print(current_ball_handler + "can either pass or shoot")
         action = input("What does " + current_ball_handler + "choose to do: ")
 
-
         #going to make it tell if the player missed the shot or not
         before_the_shot = team_2_points
 
-
         team_2_points += play(current_ball_handler,action,team_1_players,team_2_players)
-
 
         #going to make it tell if the player missed the shot or not
         if before_the_shot < team_1_points:
@@ -444,22 +378,14 @@ def main():
         else:
             print(current_ball_handler + "missed the shot")
 
-
-
-        print(show_name(team_2_lst) + "has", team_2_points, "points")
-    
-
+        print("{} has {} points".format(desired_team_2, team_2_points))
 
     #What do I have to do next?
     #Show the box score and who won the game
     winner(team_1_points,team_2_points,team_1_lst,team_2_lst)
 
-
-
-
     #Later I want to work on rebouding
     #find the probability of rebounding offensive and defensive 
-    
 
-
-main()
+if __name__ == '__main__':
+    main()
